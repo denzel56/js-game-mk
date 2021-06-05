47f45bc13fed1607913133f55f5a6fbf15da3416
Den <golodkov.den@gmail.com>
2021-06-05 17:05:07 +0300

    добавил на ничью и исправил недоработки

--- main.js
+++ main.js
@@ -62,26 +62,29 @@ function createPlayer(playerObj) {
     return $player;
 };
 
+function punchHits() {
+    // Получаем силу удара от 1 до 20
+    const punch = Math.ceil(Math.random() * 20);
+    return punch;
+}
+
 function changeHP(player) {
     const $playerLife = document.querySelector('.player' + player.player + ' .life');
-    // Получаем силу удара от 1 до 20
-    const punchHits = Math.ceil(Math.random() * 20);
-
+    
     // Отнимаем здоровье
-    player.hp -= punchHits;
+    player.hp -= punchHits();
 
     // Если здоровье 0 или меньше устанавливаем style.width = 0
     if (player.hp <= 0 ) { 
         $playerLife.style.width = 0;
+        player.hp = 0;
 
         // Отлючаем кнопку
         $randButton.disabled = true;
         return true;
-
-    } else {
-        // Иначе устанавливаем новое значение здоровья
-        $playerLife.style.width = player.hp + '%';
     };
+
+    $playerLife.style.width = player.hp + '%';
 };
 
 // Функция получает имя победителя и создает блок с именем победителя 
@@ -91,16 +94,28 @@ function playerWin(name) {
 
     return $winTitle;
 };
+// Функция выводит блок с сообщением о ничьей
+function doubleKill() {
+    const $winTitle = createElement('div', 'loseTitle');
+    $winTitle.innerText = name + ' DOUBLE KILL!';
+
+    return $winTitle;
+};
+
 
 $randButton.addEventListener('click', function() {
-    // Если игрок 1 проиграл выводим имя 2 игрока
-    if (changeHP(player1)) {
+    const p1 = changeHP(player1);
+    const p2 = changeHP(player2);
+
+    if (p1 && p2) {
+        // Если ничья выводим сообшение double kill
+        $arenas.appendChild(doubleKill());
+    } else if (p1) {
+        // Если игрок 1 проиграл выводим имя 2 игрока
         $arenas.appendChild(playerWin(player2.name));
-    };
-    
-    // Если игрок 2 проиграл выводим имя 1 игрока
-    if (changeHP(player2)) {
-       $arenas.appendChild(playerWin(player1.name));
+    } else if (p2) {
+        // Если игрок 2 проиграл выводим имя 1 игрока
+        $arenas.appendChild(playerWin(player1.name));
     };
 });
 //Вызываем функцию создания игрока

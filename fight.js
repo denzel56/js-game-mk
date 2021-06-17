ac8438ebfd0ac0a0fd5b95ddd2a1d64824909c5a
Den <golodkov.den@gmail.com>
2021-06-17 14:11:40 +0300

    homework-8

--- fight.js
+++ fight.js
@@ -1,187 +1,122 @@
 // Подключаем функции и переменные
-import getRandom from './utils.js';
-import {$fightForm, $arenas} from './main.js'
-import { createElement } from './utils.js';
-import { generateLogs, getLogMessage } from './fight_logs.js';
+// import getRandom from './utils/index.js';
+// import {$fightForm, $arenas} from './main.js'
+// import { createElement } from './utils/index.js';
+// import { generateLogs, getLogMessage } from './fight_logs.js';
+// import { ATTACK, HIT } from './constants/index.js';
+// import Player from './player/index.js';
 
-export const player1 = {
-    player: 1,
-    name: 'SUBZERO',
-    hp: 100,
-    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
-    weapon: [
-        'sword',
-        'shuriken',
-        'kunai'
-    ],
-    attack
-};
+// export const player1 = new Player({
+//     player: 1,
+//     name: 'SUBZERO',
+//     hp: 100,
+//     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+//     rootSelector: 'arenas'
+// });
 
-export const player2 = {
-    player: 2,
-    name: 'LIU KANG',
-    hp: 100,
-    img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
-    weapon: [
-        'sword',
-        'shuriken',
-        'kunai'
-    ],
-    attack
-};
+// export const player2 = new Player({
+//     player: 2,
+//     name: 'LIU KANG',
+//     hp: 100,
+//     img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
+//     rootSelector: 'arenas'
+// });
 
-function attack() {
-    return console.log('Fight...' + this.name);
-};
 
-Object.prototype.changeHP = function(hit) {
-    // Отнимаем здоровье
-    this.hp -= hit;
-
-    // Если здоровье 0 или меньше устанавливаем hp = 0
-    if (this.hp <= 0 ) { 
-        this.hp = 0;
-    };
-};
-
-Object.prototype.elHP = function() {
-    // Получаем блок здоровья игрока
-    const $player = document.querySelector(`.player${this.player} .life`);
-    
-    return $player;
-};
-
-// const elHP = () => document.querySelector(`.player${this.player} .life`);
-
-Object.prototype.renderHP = function() {
-    // Отображаем изменение шкалы здоровья
-    const $playerLife = this.elHP();
-
-    $playerLife.style.width = `${this.hp}%`;
-};
-
-export const HIT = {
-    head: 30,
-    body: 25,
-    foot: 20,
-}
-export const ATTACK = [
-    'head',
-    'body',
-    'foot'
-];
 
 // Функция получает имя победителя и создает блок с именем победителя 
-function playerWin(name) {
-    const $winTitle = createElement('div', 'loseTitle');
-    if (name) {
-        $winTitle.innerText = `${name} WINS!`;
-    } else {
-        // Выводим блок с сообщением о ничьей
-        $winTitle.innerText = 'DOUBLE KILL!';
-    };
+// function playerWin(name) {
+//     const $winTitle = createElement('div', 'loseTitle');
+//     if (name) {
+//         $winTitle.innerText = `${name} WINS!`;
+//     } else {
+//         // Выводим блок с сообщением о ничьей
+//         $winTitle.innerText = 'DOUBLE KILL!';
+//     };
 
-    return $winTitle;
-};
+//     return $winTitle;
+// };
 
-function getWinner() {
-    if (player1.hp === 0 && player1.hp < player2.hp) {
-        // Если игрок 1 проиграл выводим имя 2 игрока и генерируем сообщение в лог
-        $arenas.appendChild(playerWin(player2.name));
-        generateLogs(getLogMessage('end', player2.name, player1.name));
+// function getWinner() {
+//     if (player1.hp === 0 && player1.hp < player2.hp) {
+//         // Если игрок 1 проиграл выводим имя 2 игрока и генерируем сообщение в лог
+//         $arenas.appendChild(playerWin(player2.name));
+//         generateLogs(getLogMessage('end', player2.name, player1.name));
 
-    } else if (player2.hp === 0 && player2.hp < player1.hp) {
-        // Если игрок 2 проиграл выводим имя 1 игрока и генерируем сообщение в лог
-        $arenas.appendChild(playerWin(player1.name));
-        generateLogs(getLogMessage('end', player1.name, player2.name));
+//     } else if (player2.hp === 0 && player2.hp < player1.hp) {
+//         // Если игрок 2 проиграл выводим имя 1 игрока и генерируем сообщение в лог
+//         $arenas.appendChild(playerWin(player1.name));
+//         generateLogs(getLogMessage('end', player1.name, player2.name));
 
-    } else if (player1.hp === 0 && player2.hp === 0) {
-        // Если ничья выводим сообшение double kill и генерируем сообщение в лог
-        $arenas.appendChild(playerWin());
-        generateLogs(getLogMessage('draw'));
-    };
-};
+//     } else if (player1.hp === 0 && player2.hp === 0) {
+//         // Если ничья выводим сообшение double kill и генерируем сообщение в лог
+//         $arenas.appendChild(playerWin());
+//         generateLogs(getLogMessage('draw'));
+//     };
+// };
 
-function createReloadButton() {
-    // Создаем кконтейнер и кнопку перезагрузки
-    const $reloadWrap = createElement('div', 'reloadWrap');
-    const $reloadButton = createElement('button', 'button');
+// function createReloadButton() {
+//     // Создаем кконтейнер и кнопку перезагрузки
+//     const $reloadWrap = createElement('div', 'reloadWrap');
+//     const $reloadButton = createElement('button', 'button');
 
-    $reloadButton.innerText = 'Restart';
+//     $reloadButton.innerText = 'Restart';
 
-    // Добавляем кнопку в DOM-дерево
-    $reloadWrap.appendChild($reloadButton);
-    $arenas.appendChild($reloadWrap);
+//     // Добавляем кнопку в DOM-дерево
+//     $reloadWrap.appendChild($reloadButton);
+//     $arenas.appendChild($reloadWrap);
 
-    // Обрабатываем клик по кнопке
-    $reloadButton.addEventListener('click', function() {
-        // Обновляем страницу
-        window.location.reload();
-    });
-};
+//     // Обрабатываем клик по кнопке
+//     $reloadButton.addEventListener('click', function() {
+//         // Обновляем страницу
+//         window.location.reload();
+//     });
+// };
 
-// $randButton.addEventListener('click', function() {
-//     // Вызываем метод изменения здоровья
-//     player1.changeHP(getRandom(20));
-//     // Вызываем метод отображения здоровья
-//     player1.renderHP();
-
-//     player2.changeHP(getRandom(20))
-//     player2.renderHP();
-    
+// function showResult() {
 //     if (player1.hp === 0 || player2.hp === 0) {
-//         // Отлючаем кнопку
-//         $randButton.disabled = true;
+//         // Отлючаем кнопку и выбор действий
+//         for (let item of $fightForm) {
+
+//             if (item.type === 'submit' || item.type === 'radio') {
+//                 item.disabled = true;
+//             }
+//         }
+//         // Выводим победителя
 //         getWinner();
 //         // Вызываем фунцию создания кнопки перезагрузки
 //         createReloadButton();
 //     }
-// });
-
-export function showResult() {
-    if (player1.hp === 0 || player2.hp === 0) {
-        // Отлючаем кнопку и выбор действий
-        for (let item of $fightForm) {
-
-            if (item.type === 'submit' || item.type === 'radio') {
-                item.disabled = true;
-            }
-        }
-        // Выводим победителя
-        getWinner();
-        // Вызываем фунцию создания кнопки перезагрузки
-        createReloadButton();
-    }
-};
+// };
 
 
-export function enemyAttack() {
-    const enemyHit = ATTACK[getRandom(3) - 1];
-    const enemyDefence = ATTACK[getRandom(3) - 1];
+// export function enemyAttack() {
+//     const enemyHit = ATTACK[getRandom(3) - 1];
+//     const enemyDefence = ATTACK[getRandom(3) - 1];
 
-    return {
-        enemyHitValue: getRandom(HIT[enemyHit]),
-        enemyHit,
-        enemyDefence,
-    }
-};
+//     return {
+//         enemyHitValue: getRandom(HIT[enemyHit]),
+//         enemyHit,
+//         enemyDefence,
+//     }
+// };
 
-export function playerAttack() {
-    const attack = {};
+// export function playerAttack() {
+//     const attack = {};
 
-    for (let item of $fightForm) {
-        if (item.checked && item.name === 'hit') {
-            attack.playerHitValue = getRandom(HIT[item.value]);
-            attack.playerHit = item.value;
-        }
+//     for (let item of $fightForm) {
+//         if (item.checked && item.name === 'hit') {
+//             attack.playerHitValue = getRandom(HIT[item.value]);
+//             attack.playerHit = item.value;
+//         }
 
-        if (item.checked && item.name === 'defence') {
-            attack.playerDefence = item.value;
-        }
+//         if (item.checked && item.name === 'defence') {
+//             attack.playerDefence = item.value;
+//         }
 
-        item.checked = false;
-    };
+//         item.checked = false;
+//     };
 
-    return attack;
+//     return attack;
 
-};
+// };
--- main.js
+++ main.js
@@ -1,79 +1,49 @@
 // Подключаем функции и переменные
-import { player1, player2, showResult, enemyAttack, playerAttack } from './fight.js';
-import {createElement} from './utils.js';
-import { generateLogs, getLogMessage } from './fight_logs.js';
+import Game from './game/index.js';
+// import { player1, player2, showResult, enemyAttack, playerAttack } from './fight.js';
+// import {createElement} from './utils/index.js';
+// import { generateLogs, getLogMessage } from './fight_logs.js';
 
-export const $arenas = document.querySelector('.arenas');
 // const $randButton = document.querySelector('.button');
-export const $fightForm = document.querySelector('.control');
+// export const $arenas = document.querySelector('.arenas');
+// export const $fightForm = document.querySelector('.control');
 
+// $fightForm.addEventListener('submit', function(event) {
+//     event.preventDefault();
 
+//     const {enemyHitValue, enemyHit, enemyDefence} = enemyAttack();
+//     const {playerHitValue, playerHit, playerDefence} = playerAttack();
 
+//     // // Деструтурируем объекты
+//     // const  = enemy;
+//     // const  = player;
 
-function createPlayer(playerObj) {
-    const $player = createElement('div', `player${playerObj.player}`);
-    const $progressbar = createElement('div', 'progressbar');
-    const $life = createElement('div', 'life');
-    const $name = createElement('div', 'name');
-    const $character = createElement('div', 'character');
-    const $img = createElement('img');
+//     if (playerHit !== enemyDefence) {
+//         player2.changeHP(playerHitValue);
+//         player2.renderHP();
+//         // Получаем сообщение лога и передаем его для вывода
+//         generateLogs(getLogMessage('hit', player1.name, player2.name), playerHitValue, player2.hp);
+
+//     } else {
+//         generateLogs(getLogMessage('defence', player1.name, player2.name), 0, player2.hp);
+//     }
     
-    $life.style.width = `${playerObj.hp}%`;
-    $name.innerText = playerObj.name;
-    $img.src = playerObj.img;
+//     if (playerDefence !== enemyHit) {
+//         player1.changeHP(enemyHitValue);
+//         player1.renderHP();
+//         // Получаем сообщение лога и передаем его для вывода
+//         generateLogs(getLogMessage('hit', player2.name, player1.name), enemyHitValue, player1.hp);
 
-    $player.appendChild($progressbar);
-    $player.appendChild($character);
+//     } else {
+//         generateLogs(getLogMessage('defence', player2.name, player1.name), 0, player1.hp);
+//     }
+//     // Показываем результат
+//     showResult();
 
-    $progressbar.appendChild($life);
-    $progressbar.appendChild($name);
-
-    $character.appendChild($img);
-    
-    return $player;
-};
-
+//     // console.log('###: p', playerAttack());
+//     // console.log('###: e', enemyAttack());
+// });
 
 
-
-$fightForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const enemy = enemyAttack();
-    const player = playerAttack();
-
-    // Деструтурируем объекты
-    const {enemyHitValue, enemyHit, enemyDefence} = enemy;
-    const {playerHitValue, playerHit, playerDefence} = player;
-
-    if (playerHit !== enemyDefence) {
-        player2.changeHP(playerHitValue);
-        player2.renderHP();
-        // Получаем сообщение лога и передаем его для вывода
-        generateLogs(getLogMessage('hit', player1.name, player2.name), playerHitValue, player2.hp);
-
-    } else {
-        generateLogs(getLogMessage('defence', player1.name, player2.name), 0, player2.hp);
-    }
-    
-    if (playerDefence !== enemyHit) {
-        player1.changeHP(enemyHitValue);
-        player1.renderHP();
-        // Получаем сообщение лога и передаем его для вывода
-        generateLogs(getLogMessage('hit', player2.name, player1.name), enemyHitValue, player1.hp);
-
-    } else {
-        generateLogs(getLogMessage('defence', player2.name, player1.name), 0, player1.hp);
-    }
-    // Показываем результат
-    showResult();
-
-    console.log('###: p', player);
-    console.log('###: e', enemy);
-});
-//Вызываем функцию создания игрока
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
-// Получаем сообщение лога о старте и передаем его для вывода
-generateLogs(getLogMessage('start', player1.name, player2.name));
-
+const game = new Game();
+game.start();

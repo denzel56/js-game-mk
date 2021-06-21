2c552219a97ef2449e83aca321fbc9e058666cde
Den <golodkov.den@gmail.com>
2021-06-21 15:39:15 +0300

    https

--- game/index.js
+++ game/index.js
@@ -150,7 +150,7 @@ class Game {
     };
 
     attack = async (hit, defence) => {
-        const playersTactic = fetch('http://reactmarathon-api.herokuapp.com/api/mk/player/fight', {
+        const playersTactic = fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/fight', {
                 method: 'POST',
                 body: JSON.stringify({
                     hit,
